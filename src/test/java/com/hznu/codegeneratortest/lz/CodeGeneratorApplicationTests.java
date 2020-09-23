ffcc83286ec67f12c3ebbcec08151aa44094acf4
CRMLF <2869083541@qq.com>
2020-09-23 10:28:17 +0800

    init

--- src/test/java/com/hznu/codegeneratortest/lz/CodeGeneratorApplicationTests.java
+++ src/test/java/com/hznu/codegeneratortest/lz/CodeGeneratorApplicationTests.java
@@ -24,7 +24,6 @@ class CodeGeneratorApplicationTests {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
 
-
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
